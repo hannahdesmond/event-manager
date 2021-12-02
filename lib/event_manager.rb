422d090dc2bc193bbacce78c7718ba46e4b61c55
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-02 13:39:42 +0000

    Create clean_zipcode method to ensure zipcodes are 5 digits in length

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,5 +5,19 @@ contents = CSV.open('event_attendees.csv',
   headers: true, header_converters: :symbol)
 contents.each do |row|
   name = row[:first_name]
-  puts name
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name}, #{zipcode}"
+end
+
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  else
+    zipcode
+  end
 end
