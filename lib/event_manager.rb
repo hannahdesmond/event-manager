d975a61b1a34c6fc93f056d1a8d3cdfdd29128d9
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-02 11:57:11 +0000

    Access column using #each and CSV.open

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,8 @@
+require 'csv'
 puts 'Event Manager Initialised!'
 
-lines = File.readlines('event_attendees.csv')
-row_index = 0
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
   puts name
 end
-# This solves the problem if the header row were to change in the future. 
-# It assumes that the header row is the first row in the file.
