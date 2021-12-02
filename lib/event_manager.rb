bd1244ce205f49454c57c9f2c9e517bf9ba131df
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-02 11:39:16 +0000

    Read a column using each

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,11 @@
 puts 'Event Manager Initialised!'
 
 lines = File.readlines('event_attendees.csv')
+row_index = 0
 lines.each do |line|
-  puts line
+  row_index += 1
+  next if row_index == 1
+  columns = line.split(",")
+  name = columns[2]
+  puts name
 end
